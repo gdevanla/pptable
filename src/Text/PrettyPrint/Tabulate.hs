a5ca928feafb1a20c78b845ce14ab5b8d4d77017
Guru Devanla <gdevanla@users.noreply.github.com>
2018-03-06 21:50:46 -0800

    Refactor and Reuse methods across List, Vector and Map containers

--- src/Text/PrettyPrint/Tabulate.hs
+++ src/Text/PrettyPrint/Tabulate.hs
@@ -18,12 +18,12 @@
 -- | Module implements the default methods for Tabulate
 
 module Text.PrettyPrint.Tabulate
-    -- (
-    --   Tabulate(..)
-    -- , Boxable(..)
-    -- , CellValueFormatter
-    -- , (...)
-    -- )
+  --(
+  -- Tabulate(..)
+  -- , Boxable(..)
+  -- , CellValueFormatter
+  -- , HTrue
+  --)
 where
 
 import Data.Maybe
@@ -107,102 +107,6 @@ instance (CellValueFormatter a) => RecordMeta' HFalse a where
   toTree'' _ a = [Node (ppFormatter a) []]
 
 
-printPath :: Tree a -> [[a]]
-printPath (Node r []) = [[r]]
-printPath (Node r f) = let
-  p = [r:x | x <- (L.concatMap printPath f)]
-  in
-  p
-
-fillPath paths = stripped_paths where
-  depth = L.maximum $ L.map L.length paths
-  diff = L.map (\p -> depth - (L.length p)) paths
-  new_paths = L.map (\(p,d) ->  p ++ L.replicate d "-") $ L.zip paths diff
-  stripped_paths = [xs | x:xs <- new_paths]
-
-countLeaves :: Tree a -> Tree (Int, a)
-countLeaves (Node r f) = case f of
-  [] -> Node (1, r) []
-  x -> countLeaves' x where
-    countLeaves' x  = let
-      count_leaves = fmap countLeaves x
-      level_count = Prelude.foldr (\(Node (c, a) _) b -> c + b) 0 count_leaves
-      in
-      Node (level_count, r) count_leaves
-
-trimTree (Node r f) = trimLeaves r f
-
-trimLeaves r f = Node r (trimLeaves' f) where
-  trimLeaves' f =
-    let result = fmap trimLeaves'' f where
-          trimLeaves'' (Node r' f') = let
-            result' = case f' of
-              [] -> Nothing
-              _ -> Just $ trimLeaves r' f'
-            in
-            result'
-    in
-      catMaybes result
-
-getLeaves :: (CellValueFormatter a) => Tree a -> [String]
-getLeaves (Node r f) = case f of
-  [] -> [(ppFormatter r)]
-  _ -> foldMap getLeaves f
-
-data T = C1 { aInt::Int, aString::String} deriving (Data, Typeable, Show,G.Generic)
-data T1 = C2 { t1:: T, bInt::Float, bString::String} deriving (Data, Typeable, Show, G.Generic)
-
-c1 = C1 10 "record_c1fdsafaf"
-c2 = C2 c1 100.12121 "record_c2"
-c3 = C2 c1 10010101.12111 "record_c2fdsafdsafsafdsafasfa"
-c4 = C2 c1 1001010010101.12121 "r"
-
-instance Tabulate T HTrue
-instance Tabulate T1 HTrue
-instance CellValueFormatter T
-
-data R2 = R2 {a::Maybe Integer} deriving (G.Generic, Show)
-data R3 = R3 {r31::Maybe Integer, r32::String} deriving (G.Generic, Show)
-tr =  Node "root" (toTree . G.from $ c2)
-r2 = Node "root" (toTree . G.from $ (R2 (Just 10)))
-r3 = Node "root" (toTree . G.from $ (R3 (Just 10) "r3_string"))
-
-showTree :: (Show a) => Tree a -> Tree String
-showTree (Node r f) = case f of
-  [] -> Node (show r) []
-  x -> showTree' x where
-    showTree' x = let
-      show_children = fmap showTree x
-      in
-      Node (show r) show_children
-
-createBoxedHeaders :: [[String]] -> [B.Box]
-createBoxedHeaders paths = boxes where
-  boxes = L.map wrapWithBox paths
-  wrapWithBox p = B.vsep 0 B.top $ L.map B.text p
-
-
--- TODO: Consolidate these 2 methods
-ppRecords :: (GRecordMeta (Rep a), G.Generic a) => [a] -> IO ()
-ppRecords recs = result where
-  tr =  fmap (\a -> Node "root" $ (toTree . G.from $ a)) recs
-  headers =  createBoxedHeaders . fillPath . printPath . trimTree . L.head $ tr
-  horizontal_boxes =  fmap (fmap  B.text) $ fmap getLeaves tr
-  cols = fmap (B.vsep 0 B.top) $ L.transpose horizontal_boxes
-  result = B.printBox $ B.hsep 5 B.top $ fmap (\(a, b) -> B.vsep 0 B.top $ [a, b]) $ L.zip headers cols
-
-ppRecordsWithIndex :: (CellValueFormatter k, GRecordMeta (Rep a), G.Generic a) => (Map.Map k a) -> IO ()
-ppRecordsWithIndex recs = result where
-  tr =  fmap (\a -> Node "root" $ (toTree . G.from $ a)) $ Map.elems recs
-  headers =  createBoxedHeaders . fillPath . printPath . trimTree . L.head $ tr
-  horizontal_boxes =  fmap (fmap  B.text) $ fmap getLeaves tr
-  cols = fmap (B.vsep 0 B.top) $ L.transpose horizontal_boxes
-  -- build index
-  header_length = L.length . L.head . fillPath . printPath . trimTree . L.head $ tr
-  index_col = (L.replicate header_length "-" ) ++  (L.map ppFormatter $ Map.keys recs)
-  index_box = B.vsep 0 B.top $ L.map B.text index_col
-  data_box = fmap (\(a, b) -> B.vsep 0 B.top $ [a, b]) $ L.zip headers cols
-  result = B.printBox $ B.hsep 5 B.top $ index_box:data_box
 
 -- |  Class that implements formatting using printf.
 --    Default instances for String, Char, Int, Integer, Double and Float
@@ -290,3 +194,115 @@ instance (CellValueFormatter k) => Boxable (Map.Map k) where
   -- | Prints a "Map" as a table. Called by "ppTable"
   -- | Need not be called directly
   printTable m = ppRecordsWithIndex m
+
+
+
+-- Internal helper functions
+
+printPath :: Tree a -> [[a]]
+printPath (Node r []) = [[r]]
+printPath (Node r f) = let
+  p = [r:x | x <- (L.concatMap printPath f)]
+  in
+  p
+
+fillPath paths = stripped_paths where
+  depth = L.maximum $ L.map L.length paths
+  diff = L.map (\p -> depth - (L.length p)) paths
+  new_paths = L.map (\(p,d) ->  p ++ L.replicate d "-") $ L.zip paths diff
+  stripped_paths = [xs | x:xs <- new_paths]
+
+countLeaves :: Tree a -> Tree (Int, a)
+countLeaves (Node r f) = case f of
+  [] -> Node (1, r) []
+  x -> countLeaves' x where
+    countLeaves' x  = let
+      count_leaves = fmap countLeaves x
+      level_count = Prelude.foldr (\(Node (c, a) _) b -> c + b) 0 count_leaves
+      in
+      Node (level_count, r) count_leaves
+
+trimTree (Node r f) = trimLeaves r f
+
+trimLeaves r f = Node r (trimLeaves' f) where
+  trimLeaves' f =
+    let result = fmap trimLeaves'' f where
+          trimLeaves'' (Node r' f') = let
+            result' = case f' of
+              [] -> Nothing
+              _ -> Just $ trimLeaves r' f'
+            in
+            result'
+    in
+      catMaybes result
+
+getLeaves :: (CellValueFormatter a) => Tree a -> [String]
+getLeaves (Node r f) = case f of
+  [] -> [(ppFormatter r)]
+  _ -> foldMap getLeaves f
+
+data T = C1 { aInt::Int, aString::String} deriving (Data, Typeable, Show,G.Generic)
+data T1 = C2 { t1:: T, bInt::Float, bString::String} deriving (Data, Typeable, Show, G.Generic)
+
+c1 = C1 1000 "record_c1fdsafaf"
+c2 = C2 c1 100.12121 "record_c2"
+c3 = C2 c1 10010101.12111 "record_c2fdsafdsafsafdsafasfa"
+c4 = C2 c1 1001010010101.12121 "r"
+
+instance Tabulate T HTrue
+instance Tabulate T1 HTrue
+instance CellValueFormatter T
+
+data R2 = R2 {a::Maybe Integer} deriving (G.Generic, Show)
+data R3 = R3 {r31::Maybe Integer, r32::String} deriving (G.Generic, Show)
+tr =  Node "root" (toTree . G.from $ c2)
+r2 = Node "root" (toTree . G.from $ (R2 (Just 10)))
+r3 = Node "root" (toTree . G.from $ (R3 (Just 10) "r3_string"))
+
+showTree :: (Show a) => Tree a -> Tree String
+showTree (Node r f) = case f of
+  [] -> Node (show r) []
+  x -> showTree' x where
+    showTree' x = let
+      show_children = fmap showTree x
+      in
+      Node (show r) show_children
+
+ppRecords :: (GRecordMeta (Rep a), G.Generic a) => [a] -> IO ()
+ppRecords recs = result where
+  result = B.printBox $ B.hsep 5 B.top $ createHeaderDataBoxes recs
+
+ppRecordsWithIndex :: (CellValueFormatter k, GRecordMeta (Rep a), G.Generic a) => (Map.Map k a) -> IO ()
+ppRecordsWithIndex recs = result where
+  data_boxes = createHeaderDataBoxes $ Map.elems recs
+  index_box = createIndexBoxes recs
+  result = B.printBox $ B.hsep 5 B.top $ index_box:data_boxes
+
+recsToTrees recs = fmap (\a -> Node "root" $ (toTree . G.from $ a)) $ recs
+
+getHeaderDepth rec_trees = header_depth where
+  header_depth = L.length . L.head . fillPath . printPath . trimTree . L.head $ rec_trees
+
+createBoxedHeaders :: [[String]] -> [B.Box]
+createBoxedHeaders paths = boxes where
+  boxes = L.map wrapWithBox paths
+  wrapWithBox p = B.vsep 0 B.top $ L.map B.text p
+
+createHeaderCols rec_trees = header_boxes where
+  header_boxes =  createBoxedHeaders . fillPath . printPath . trimTree . L.head $ rec_trees
+
+createDataBoxes rec_trees = vertical_boxes where
+  horizontal_boxes =  fmap (fmap  B.text) $ fmap getLeaves rec_trees
+  vertical_boxes = fmap (B.vsep 0 B.top) $ L.transpose horizontal_boxes
+
+createIndexBoxes recs = index_box where
+  rec_trees = recsToTrees $ Map.elems recs
+  header_depth = getHeaderDepth rec_trees
+  index_col = (L.replicate header_depth "-" ) ++  (L.map ppFormatter $ Map.keys recs)
+  index_box = B.vsep 0 B.top $ L.map B.text index_col
+
+createHeaderDataBoxes recs = vertical_boxes where
+  rec_trees = recsToTrees recs
+  header_boxes = createHeaderCols rec_trees
+  data_boxes = createDataBoxes rec_trees
+  vertical_boxes = fmap (\(a, b) -> B.vsep 0 B.top $ [a, b]) $ L.zip header_boxes data_boxes
